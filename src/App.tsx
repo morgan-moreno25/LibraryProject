a62763216e372abd906531f902da561a7721dd89
Morgan <morgan.moreno25@gmail.com>
2021-03-16 12:13:12 -0500

    Set up basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import { Navbar } from "react-bootstrap";
+import { Code, GitHub, Linkedin } from "react-feather";
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div id="app">
+      <Navbar bg="primary" variant="dark" id="header">
+        <h1 className="font-weight-bold">Library</h1>
+      </Navbar>
+      <main id="content" className="container"></main>
+      <footer id="footer" className="container-fluid">
+        <div
+          id="footer-links"
+          className="row justify-content-center align-items-center h-100"
+        >
+          <a href="#!" className="col-3 btn btn-primary mx-2">
+            <GitHub />
+          </a>
+          <a href="#!" className="col-3 btn btn-primary mx-2">
+            <Linkedin />
+          </a>
+          <a href="#!" className="col-3 btn btn-primary mx-2">
+            <Code />
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
 }
